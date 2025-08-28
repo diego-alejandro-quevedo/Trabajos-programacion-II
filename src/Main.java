9aa1ef271ea4453c22775ca33e417f6b72f53128
DiegoQ <diegoaquevedo@ucundinamarca.edu.co>
2025-08-28 17:06:53 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,17 @@ public class Main {
 
         System.out.println("hola mundoo");
         System.out.println("Diego Alejandro Quevedo");
+
+        System.out.println("GIT STATUS");
+        System.out.println(" GIT BRANCH");
+        System.out.println("GIT ADD .");
+        System.out.println("GIT PUSH ORIGIN");
+        System.out.println("GIT COMMIT -M");
+        System.out.println("GIT CONFIG --LIST");
+        System.out.println("GIT LOG");
+        System.out.println("GIT CLONE");
+        System.out.println("GIT FETCH");
+        System.out.println("GIT SWITCH -");
+
     }
 }
