62e321f564c61979794b307b4e4d5e95a8ce0322
diegoQ <diegoaquevedo@ucundinamarca.edu.co>
2025-09-05 12:32:44 -0500

    crea ejercicio de lanzar 3 dados 20 veces sumar los resultados y comparar mayor,medio,menor

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,64 @@
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hola mundoo");
-        System.out.println("Diego Alejandro Quevedo");
+        Random random= new Random();
 
-        System.out.println("GIT STATUS");
-        System.out.println(" GIT BRANCH");
-        System.out.println("GIT ADD .");
-        System.out.println("GIT PUSH ORIGIN");
-        System.out.println("GIT COMMIT -M");
-        System.out.println("GIT CONFIG --LIST");
-        System.out.println("GIT LOG");
-        System.out.println("GIT CLONE");
-        System.out.println("GIT FETCH");
-        System.out.println("GIT SWITCH -c");
+        int dado1,dado2,dado3,sum1,sum2, sum3;
 
-        System.out.println("GIT CLONE");
-        System.out.println("GIT FETCH");
-        System.out.println("GIT SWITCH -c");
+        sum1=0;
+        sum2=0;
+        sum3=0;
+
+        for(int i=1;i<=20;i++){
+
+            dado1 = random.nextInt((6 - 1)+1)+1;
+            sum1=sum1+dado1;
+
+
+            dado2 = random.nextInt((6 - 1)+1)+1;
+            sum2=sum2+dado2;
+
+            dado3 = random.nextInt((6 - 1)+1)+1;
+            sum3=sum3+dado3;
+
+        }
+
+         if(sum1>sum2 && sum1>sum3){
+            System.out.println("el dado uno es el mayor "+sum1);
+            if (sum2>sum3){
+                System.out.println("el dado dos es el del medio "+sum2);
+                System.out.println("el dado tres es el menor "+sum3);
+            }
+            else {
+                System.out.println("el dado tres es el del medio "+sum3);
+                System.out.println("el dado dos es el menor "+sum2);
+            }
+        }
+        else if(sum2>sum3){
+             System.out.println("el dado dos es el mayor "+sum2);
+             if (sum1>sum3){
+                 System.out.println("el dado uno es el del medio "+sum1);
+                 System.out.println("el dado tres es el menor "+sum3);
+             }
+             else {
+                 System.out.println("el dado tres es el del medio "+sum3);
+                 System.out.println("el dado uno es el menor "+sum1);
+             }
+        }
+        else {
+             System.out.println("el dado tres es el mayor "+sum3);
+             if (sum1>sum2){
+                 System.out.println("el dado uno es el del medio "+sum1);
+                 System.out.println("el dado dos es el menor "+sum2);
+             }
+             else {
+                 System.out.println("el dado dos es el del medio "+sum2);
+                 System.out.println("el dado uno es el menor "+sum1);
+             }
+         }
 
 
     }
